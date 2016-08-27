2181188701291ea6a4437f9b37788080568dee59
hlecorche <hlecorche@e-commit.fr>
2016-08-27 10:40:27 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,64 @@
+CSV Table Generator
+====================
+
+Create a CSV file with PHP array.
+
+[![SensioLabsInsight](https://insight.sensiolabs.com/projects/28c792f3-d5ac-4efb-9500-c7bf7cc06b7c/big.png)](https://insight.sensiolabs.com/projects/28c792f3-d5ac-4efb-9500-c7bf7cc06b7c)
+
+Installation
+-------------
+
+To install csv-table-generator with Composer just run :
+
+```bash
+$ composer require ecommit/csv-table-generator
+```
+
+
+
+Usage
+------
+
+```php
+use Ecommit\CsvTableGenerator\Csv;
+
+$csv = new Csv('/home/test', 'myfilename', array(
+    'header' => array(
+        'Column A',
+        'Column B',
+    ),
+));
+
+$csv->write(array('Hello', 'world')); //Add line
+$csv->write(array('Test1', 'Test2')); //Add line
+$csv->close();
+```
+
+/home/test/myfilename.csv is generated :
+
+```
+"Column A","Column B"
+Hello,world
+Test1,Test2
+```
+
+**Constructor arguments :**
+
+* **String $pathDir** : Path folder (when CSV file is generated) **Required**
+* **String $filename** : Filename (without path folder and extension) **Required**
+* **Array $options** : Options. See below
+
+**Availabled options :**
+
+* **header** (array) : Header array. If empty, no header. **Default: array()**
+* **max_lines** (null | int) : Max lines per CSV file. If lines > max_lines, many files are generated. **Default: null**
+* **delimiter** (string) : CSV delimiter. **Default: ,**
+* **enclosure** (string) : CSV enclosure. **Default: "**
+* **eol** (string - Csv::EOL_ constants) : EOF(End Of Line) character. See **Csv::EOL_** constants. **Default: Csv::EOL_LF**. If **Csv::EOL_CRLF**
+  is used, **unix2dos** program is required
+* **unix2dos_path** (string) : Unix2dos path. Only used if eol=Csv::EOL_CRLF. **Default: /usr/bin/unix2dos**
+* **add_utf8_bom** (bool) : Add or not UTF8 bom. **Default: false**
+
+##License
+
+This librairy is under the MIT license. See the complete license in *LICENSE* file.
---
+++ src/Csv.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * This file is part of the csv-table-generator package.
+ *
+ * (c) E-commit <contact@e-commit.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ecommit\CsvTableGenerator;
+
+class Csv
+{
+    const EOL_LF = 'LF';
+    const EOL_CRLF = 'CR+LF';
+
+    protected $handle;
+    protected $pathDir;
+    protected $filename;
+    protected $currentFile;
+    protected $header;
+    protected $maxLines;
+    protected $delimiter;
+    protected $enclosure;
+    protected $fileNumber = 0;
+    protected $lines;
+    protected $unixToDos = false;
+    protected $unixToDosPath;
+    protected $addUtf8Bom = false;
+
+    /**
+     * Constructor
+     *
+     * @param string $pathDir Path folder
+     * @param string $filename Filename (without path folder and extension)
+     * @param array $options See README.md
+     */
+    public function __construct($pathDir, $filename, $options = array()) {
+        $defaultOptions = array(
+            'header' => array(),
+            'max_lines' => null,
+            'delimiter' => ',',
+            'enclosure' => '"',
+            'eol' => self::EOL_LF,
+            'unix2dos_path' => '/usr/bin/unix2dos',
+            'add_utf8_bom' => false,
+        );
+        $options = array_merge($defaultOptions, $options);
+
+        //Test folder
+        $realPath = \realpath($pathDir);
+        if (false === $realPath || !\is_writable($realPath)) {
+            throw new \Exception(sprintf('Folder %s does not exist or is not writable', $pathDir));
+        }
+
+        $this->pathDir = $realPath;
+        $this->filename = $filename;
+        $this->header = $options['header'];
+        if (empty($options['max_lines'])) {
+            $this->maxLines = null;
+        } else {
+            $this->maxLines = \intval($options['max_lines']);
+        }
+        $this->delimiter = $options['delimiter'];
+        $this->enclosure = $options['enclosure'];
+
+        if (self::EOL_CRLF === $options['eol']) {
+            $this->unixToDos = true;
+        }
+        $this->unixToDosPath = $options['unix2dos_path'];
+
+        $this->addUtf8Bom = $options['add_utf8_bom'];
+
+        $this->open();
+    }
+
+    /**
+     * Open CSV file
+     */
+    protected function open()
+    {
+        if ($this->handle) {
+            throw new \Exception(sprintf('The file %s is already open', $this->filename));
+        }
+        $this->fileNumber++;
+        $this->lines = 0;
+        $filename = $this->filename;
+        if ($this->fileNumber > 1) {
+            $filename .= '-' . $this->fileNumber;
+        }
+        $this->currentFile = $this->pathDir . '/' . $filename . '.csv';
+        $this->handle = \fopen($this->currentFile, 'wb'); //Binary is forced. EOL = "\n"
+        if (false === $this->handle) {
+            throw new \Exception(sprintf('Error during the opening of the %s file', $this->filename));
+        }
+        if ($this->addUtf8Bom) {
+            if (!\fputs($this->handle, chr(0xEF).chr(0xBB).chr(0xBF))) {
+                throw new \Exception(sprintf('Error during the UTF8-BOM writing in %s file', $this->filename));
+            }
+        }
+        if (!empty($this->header)) {
+            $this->write($this->header);
+            $this->lines = 0;
+        }
+    }
+
+    /**
+     * Close CSV file
+     */
+    public function close()
+    {
+        if ($this->handle) {
+            \fclose($this->handle);
+            $this->handle = null;
+
+            if ($this->unixToDos) {
+                passthru(sprintf('%s %s', $this->unixToDosPath, $this->currentFile), $returnVar);
+                if (0 !== $returnVar) {
+                    throw new \Exception(sprintf('Unix2dos error (%s file)', $this->filename));
+                }
+            }
+            $this->currentFile = null;
+        }
+    }
+
+    /**
+     * Create new CSV file
+     */
+    protected function newFile()
+    {
+        $this->close();
+        $this->open();
+    }
+
+    /**
+     * Add line in CSV file
+     * @param array $data
+     */
+    public function write($data)
+    {
+        if (!$this->handle) {
+            throw new \Exception(sprintf('Handle does not exist. File %s', $this->filename));
+        }
+
+        //New file
+        if ($this->maxLines && $this->maxLines == $this->lines) {
+            $this->newFile();
+        }
+
+        //Write
+        if (false === \fputcsv($this->handle, $data, $this->delimiter, $this->enclosure)) {
+            throw new \Exception(sprintf('Error during the writing in %s file', $this->filename));
+        }
+
+        $this->lines++;
+    }
+}
